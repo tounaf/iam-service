67496e1615d3d3c2ed058316f94845d2636bbc88
Tounaf <harinjatovo.fetra@gmail.com>
2026-08-28 09:22:20 +0300

    webpack config prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,10 @@
 import Encore from '@symfony/webpack-encore';
 
+// S'assure que Node/Babel sait qu'on compile pour la production
+if (Encore.isProduction()) {
+    process.env.NODE_ENV = 'production';
+}
+
 Encore
     .setOutputPath('public/build/')
     .setPublicPath('/build')
@@ -9,7 +14,23 @@ Encore
     .cleanupOutputBeforeBuild()
     .enableSourceMaps(!Encore.isProduction())
     .enableVersioning(Encore.isProduction())
-    .enableReactPreset()
+
+    // On désactive le preset React automatique d'Encore pour le configurer manuellement sans conflit
+    .configureBabel((babelConfig) => {
+        // Nettoyage de tout preset React préexistant
+        babelConfig.presets = babelConfig.presets.filter(
+            preset => !(Array.isArray(preset) && preset[0].includes('preset-react'))
+        );
+
+        // Ajout du preset avec l'environnement exact
+        babelConfig.presets.push([
+            '@babel/preset-react',
+            {
+                runtime: 'automatic',
+                development: !Encore.isProduction(),
+            }
+        ]);
+    })
 ;
 
 export default Encore.getWebpackConfig();
